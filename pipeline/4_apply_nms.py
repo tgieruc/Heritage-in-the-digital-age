8267f202139c8732817a7cc29fe4dec7929e4076
Théo Gieruc <theo.gieruc@gmail.com>
2023-01-09 19:32:02 +0100

    fixed for python <3.9

--- pipeline/4_apply_nms.py
+++ pipeline/4_apply_nms.py
@@ -37,7 +37,7 @@ def labelwise_nms(segmentation_results, iou_th=0.2):
         boxes += (segmentation_results[1][idx_][idx_tokeep])
         captions += (
             [segmentation_results[2][i] for i in torch.index_select(torch.tensor(idx_), 0, idx_tokeep)])
-    captions = [caption[:1] if caption[0] == ' ' else caption for caption in captions]
+    captions = [caption[1:] if caption[0] == ' ' else caption for caption in captions]
     # Make the new segmentation
     segmentation_nms = []
     segmentation_nms.append(torch.stack(scores, dim=0))
@@ -60,7 +60,7 @@ def global_nms(segmentation_results, iou_th=0.9):
     scores += (segmentation_results[0][idx_to_keep])
     boxes += (segmentation_results[1][idx_to_keep])
     captions += ([segmentation_results[2][i] for i in idx_to_keep])
-    captions = [caption[:1] if caption[0] == ' ' else caption for caption in captions]
+    captions = [caption[1:] if caption[0] == ' ' else caption for caption in captions]
 
     # Make the new segmentation
     segmentation_nms = []
