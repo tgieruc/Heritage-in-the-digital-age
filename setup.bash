566db3c6eef322d7576e5847c90c6cc729bfeb8e
tgieruc <theo.gieruc@gmail.com>
2023-04-19 16:29:22 +0200

    added sentencepiece to requirements

--- setup.bash
+++ setup.bash
@@ -3,15 +3,26 @@
 # DINO
 HOME=$PWD
 
+echo "Downloading DINO model..."
+
 mkdir -p $HOME/submodules/GroundingDINO/model
 wget https://github.com/IDEA-Research/GroundingDINO/releases/download/v0.1.0-alpha/groundingdino_swint_ogc.pth -O $PWD/submodules/GroundingDINO/model/groundingdino_swint_ogc.pth
 
 cd $HOME/submodules/GroundingDINO
 
+echo 
+echo "Installing DINO requirements..."
+
 pip install -q -e .
 
 cd $HOME
+
+echo 
+echo "Installing Segment Anything..."
 # Segment Anything
 pip install -q git+https://github.com/facebookresearch/segment-anything.git
 
+
+echo
+echo "Installing last requirements..."
 pip install -q -r requirements.txt
