58af184713884e73b3b3fe41b377a2210b8819ae
tgieruc <theo.gieruc@gmail.com>
2023-04-19 16:01:16 +0200

    Create model folder if not existing

--- setup.bash
+++ setup.bash
@@ -3,6 +3,7 @@
 # DINO
 HOME=$PWD
 
+mkdir -p $HOME/submodules/GroundingDINO/model
 wget https://github.com/IDEA-Research/GroundingDINO/releases/download/v0.1.0-alpha/groundingdino_swint_ogc.pth -O $PWD/submodules/GroundingDINO/model/groundingdino_swint_ogc.pth
 
 cd $HOME/submodules/GroundingDINO
